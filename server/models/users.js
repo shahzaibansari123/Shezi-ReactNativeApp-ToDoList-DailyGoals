dc6697f6fca33a9ccc072b01f71f883524f35b63
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-09 23:16:30 +0500

    work complete

--- server/models/users.js
+++ server/models/users.js
@@ -5,6 +5,11 @@ const userSchema= new mongoose.Schema({
     name: {
         type: String,
         required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
     }
 })
 
