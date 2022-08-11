578f8d176cb57bc9acd1cdd7eb176c9578bba9e8
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-11 22:31:39 +0500

    work complete

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,31 +1,31 @@
-import {User} from '../models/users.js'
-import { sendMail } from '../utils/sendMail.js';
+import { User } from "../models/users.js";
+import { sendMail } from "../utils/sendMail.js";
 
-export const register=async (req, res) => {
-    try {
-        const {name, email, password}= req.body;
-        const {avatar}= req.files;
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const { avatar } = req.files;
 
-        let user= await User.findOne({email})
+    let user = await User.findOne({ email });
 
-        if (user){
-            return res
-            .status(400)
-            .json({success: false, message: "user already exists"})
-        }
-
- const otp= Math.floor(Math.random()* 1000000)
-        user= await User.create({
-            name, 
-            email, 
-            password, 
-            avatar, 
-            otp, 
-            otp_expiry: new Date(Date.now() +  process.env.OTP_EXPIRE * 60 * 1000)}) 
-
-            await sendMail(email, "Verify your account", `Your OTP is ${otp}`)
-    } catch (error) {
-        res.status(500).json({success: false, message: error.message})
-        
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "user already exists" });
     }
-}
+
+    const otp = Math.floor(Math.random() * 1000000)
+    user = await User.create({
+      name,
+      email,
+      password,
+      avatar,
+      otp,
+      otp_expiry: new Date(Date.now() + process.env.OTP_EXPIRE * 60 * 1000),
+    });
+
+    await sendMail(email, "Verify your account", `Your OTP is ${otp}`);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
