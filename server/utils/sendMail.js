353558d35454b54188a9387e24ceb57326b8ec03
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-11 21:53:00 +0500

    work complete

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,3 +1,14 @@
 import {createTransport} from  'nodemailer';
 
-export const sendMail= async (email, subject, text) => {}
+export const sendMail= async (email, subject, text) => {
+    const transport = createTransport({})
+
+    await transport.sendMail({
+        from : 'testUser',
+        to: email,
+        subject,
+        text,
+    })
+
+
+}
