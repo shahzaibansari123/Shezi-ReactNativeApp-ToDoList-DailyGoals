e69542fc88201fc38048e4aad1a0780ac57b4faa
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-12 23:17:10 +0500

    work complete

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,3 +1,13 @@
 export const sendToken=(res, user, statusCode,message)=>{
- res.statusCode(statusCode).json({success:true, message});
+
+
+
+    const userData={
+        _id: user._id,
+        name:user.name,
+        email:user.email,
+        avatar:user.avatar,
+        tasks:user.tasks,
+    }
+ res.statusCode(statusCode).json({success:true, message, userData});
 }
