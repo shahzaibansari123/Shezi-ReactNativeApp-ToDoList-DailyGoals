5fbb1f03415529bbd755bec3e0b3a51953142471
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-13 22:32:21 +0500

    work complete

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,6 +1,9 @@
 export const sendToken=(res, user, statusCode,message)=>{
 
-
+const options={
+    httpOnly: true,
+    expires: new Date(Date.now() + process.env.COOKIE_JWT_EXPIRES_AT * 60 * 1000)
+}
 
     const userData={
         _id: user._id,
@@ -9,5 +12,5 @@ export const sendToken=(res, user, statusCode,message)=>{
         avatar:user.avatar,
         tasks:user.tasks,
     }
- res.statusCode(statusCode).json({success:true, message, userData});
+ res.statusCode(statusCode).cookie("token", token, options).json({success:true, message, userData});
 }
