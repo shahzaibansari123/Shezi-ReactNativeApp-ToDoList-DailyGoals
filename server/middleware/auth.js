b6e4df027b1a3d6cb0967b8b63cdd0955b002f13
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-17 23:08:43 +0500

    work complete

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,5 +2,10 @@ import jwt from 'jsonwebtoken'
 import { User } from '../models/users'
 
 export const isAuthenticated= async(req, res)=>{
+    try {
+        const {token} = req.cookies;
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 
 }
