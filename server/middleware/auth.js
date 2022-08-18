71e55aa4a467c40612685d80a14036b71ad00830
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-18 21:54:35 +0500

    work complete

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,20 @@
-import jwt from 'jsonwebtoken'
-import { User } from '../models/users'
+import jwt from "jsonwebtoken";
+import { User } from "../models/users";
 
-export const isAuthenticated= async(req, res)=>{
-    try {
-        const {token} = req.cookies;
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
 
-        if(!token){
-            res.status(401).json({ success: false, message: "Login First" });    
-        }
-
-        const decoded= jwt.verify(token, process.env.JWT_SEcRET_KEY);
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+    if (!token) {
+      res.status(401).json({ success: false, message: "Login First" });
     }
 
-}
+    const decoded = jwt.verify(token, process.env.JWT_SEcRET_KEY);
+
+    req.user = await User.findById(decoded._id);
+
+    next();
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
