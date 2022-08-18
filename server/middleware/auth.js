33c236559c6db663d4297742e36899c825997c02
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-18 21:41:35 +0500

    work complete

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,6 +4,10 @@ import { User } from '../models/users'
 export const isAuthenticated= async(req, res)=>{
     try {
         const {token} = req.cookies;
+
+        if(!token){
+            res.status(401).json({ success: false, message: "Login First" });    
+        }
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
