ac8ff1741954f733b5f3b88449fe7191f43f5f8e
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-19 22:16:05 +0500

    work complete

--- server/routers/User.js
+++ server/routers/User.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {register, verify} from '../controllers/User.js'
+import { isAuthenticated } from "../middleware/auth.js";
 
 const router= express.Router();
 
 
 router.route('/register').post(register)
-router.route('/verify').post(verify)
+router.route('/verify').post(isAuthenticated,verify)
 
 export default router;
