947ce450bc803e3faf5a14e5d4fd85c2d4b06077
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-22 23:10:10 +0500

    work complete

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 import  express  from "express";
 import User from './routers/User.js'
+import cookieParser from "cookie-parser";
 export const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 app.use('/api/v1', User)
