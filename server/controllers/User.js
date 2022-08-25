9353454840b07883a371777480f4fb76bfd2c41d
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-25 23:27:16 +0500

    work complete

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -76,7 +76,7 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
     // const { avatar } = req.files;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("+password");
 
     if (!user) {
       return res
