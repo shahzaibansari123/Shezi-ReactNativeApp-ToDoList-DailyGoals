a29a4586cd186eac49dec929024cca8f9fe352fb
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-09-30 20:05:17 +0500

    work complete

--- todoapp/Main.jsx
+++ todoapp/Main.jsx
@@ -1,15 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Main = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-    {/* <View>
-      <Text>Main</Text>
-    </View> */}
+      <Stack.Navigator>
+
+        
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default Main;
