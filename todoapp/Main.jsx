9ce315f3ec1e0e6d09abebf6081e805db9300c46
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-01 22:35:54 +0500

    work complete

--- todoapp/Main.jsx
+++ todoapp/Main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
 
 const Main = () => {
   const Stack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ const Main = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="home">
         <Stack.Screen name="home" component={Home}/>
+        <Stack.Screen name="login" component={Login}/>
 
 
       </Stack.Navigator>
--- todoapp/screens/Home.jsx
+++ todoapp/screens/Home.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+    const navigation= useNavigation()
   return (
     <View>
-      <Text>Home</Text>
+      <Text onPress={()=> navigation.navigate("login")}>Home</Text>
     </View>
   )
 }
