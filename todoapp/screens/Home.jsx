f5ecdf5ebd2a3e7ab6eb16dba50086d6321c51a7
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-02 20:45:37 +0500

    work complete

--- todoapp/screens/Home.jsx
+++ todoapp/screens/Home.jsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+// import { useNavigation } from '@react-navigation/native'
 
-const Home = () => {
-    const navigation= useNavigation()
+const Home = ({navigation}) => {
+    // const navigation= useNavigation()
   return (
     <View>
       <Text onPress={()=> navigation.navigate("login")}>Home</Text>
