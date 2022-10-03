4de91f4224f49b8f7c7e1b3d795a813c2c9d9125
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-03 20:11:04 +0500

    work complete

--- todoapp/screens/Home.jsx
+++ todoapp/screens/Home.jsx
@@ -1,14 +1,33 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView, Text } from 'react-native'
 import React from 'react'
 // import { useNavigation } from '@react-navigation/native'
+import { StatusBar } from 'react-native'
+import {useIsFocused} from '@react-navigation/core'
+
+
+
+// const FocusedStatusBar = (props) => {
+//   const isFocused= useIsFocused()
+//   return isFocused ? <StatusBar animated={true} {...props} /> : null
+// }
 
 const Home = ({navigation}) => {
     // const navigation= useNavigation()
   return (
-    <View style={{backgroundColor: "white", flex: 1}}>
+    <SafeAreaView style={{ flex: 1}} >
+       {/* <FocusedStatusBar background={"black"}/> */}
+       <StatusBar
+        animated={true}
+        backgroundColor="#61dafb"
+        barStyle='light-content'
+        // showHideTransition={statusBarTransition}
+        hidden={false}
+        
+
+         />
       {/* <Text onPress={()=> navigation.navigate("login")}>Home</Text> */}
     <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
