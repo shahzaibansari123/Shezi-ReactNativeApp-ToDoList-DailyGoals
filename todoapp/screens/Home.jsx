9dbfe697202d01f2ed1e27134643377060b954bd
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-03 20:19:35 +0500

    work complete

--- todoapp/screens/Home.jsx
+++ todoapp/screens/Home.jsx
@@ -1,34 +1,32 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Platform, StatusBar } from "react-native";
+import React from "react";
 // import { useNavigation } from '@react-navigation/native'
-import { StatusBar } from 'react-native'
-import {useIsFocused} from '@react-navigation/core'
 
-
-
-// const FocusedStatusBar = (props) => {
-//   const isFocused= useIsFocused()
-//   return isFocused ? <StatusBar animated={true} {...props} /> : null
-// }
-
-const Home = ({navigation}) => {
-    // const navigation= useNavigation()
+const Home = ({ navigation }) => {
+  // const navigation= useNavigation()
   return (
-    <SafeAreaView style={{ flex: 1}} >
-       {/* <FocusedStatusBar background={"black"}/> */}
-       <StatusBar
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
+    >
+      {/* ======================= */}
+      {/* setting status bar height manully as safe area view doesnot work in android   */}
+      {/* <StatusBar
         animated={true}
         backgroundColor="#61dafb"
         barStyle='light-content'
         // showHideTransition={statusBarTransition}
         hidden={false}
-        
+         /> */}
+      {/* ======================= */}
 
-         />
       {/* <Text onPress={()=> navigation.navigate("login")}>Home</Text> */}
-    <Text>Home</Text>
-    </SafeAreaView>
-  )
-}
+      <Text>Home</Text>
+    </View>
+  );
+};
 
-export default Home
+export default Home;
