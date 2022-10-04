d9dc83225037af4f3a78afba5442e4cee33b91b3
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-04 19:43:46 +0500

    work complete

--- todoapp/screens/Home.jsx
+++ todoapp/screens/Home.jsx
@@ -1,32 +1,47 @@
-import { View, Text, Platform, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 // import { useNavigation } from '@react-navigation/native'
 
 const Home = ({ navigation }) => {
   // const navigation= useNavigation()
-  return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-      }}
-    >
-      {/* ======================= */}
-      {/* setting status bar height manully as safe area view doesnot work in android   */}
-      {/* <StatusBar
-        animated={true}
-        backgroundColor="#61dafb"
-        barStyle='light-content'
-        // showHideTransition={statusBarTransition}
-        hidden={false}
-         /> */}
-      {/* ======================= */}
 
-      {/* <Text onPress={()=> navigation.navigate("login")}>Home</Text> */}
-      <Text>Home</Text>
+  return (
+
+    // =======================
+    //    setting status bar height manully as safe area view doesnot work in android
+    //    <StatusBar
+    //     animated={true}
+    //     backgroundColor="#61dafb"
+    //     barStyle='light-content'
+    //     showHideTransition={statusBarTransition}
+    //     hidden={false}
+    //      />
+    //   =======================
+
+    //   <Text onPress={()=> navigation.navigate("login")}>Home</Text>
+
+    <View style={Styles.container}>
+      <SafeAreaView>
+        <Text style={}>All Tasks:</Text>
+      </SafeAreaView>
     </View>
   );
 };
+
+const Styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  heading: {},
+});
 
 export default Home;
