d461323f7b6d46c1d9df34e3dfe909a23b12fee3
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-06 21:39:53 +0500

    work complete

--- todoapp/components/Task.jsx
+++ todoapp/components/Task.jsx
@@ -1,18 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React , {useState} from 'react'
-import {checkbox} from 'react-native-paper'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { checkbox } from "react-native-paper";
 
-const Task = ({title, description, status, taskId}) => {
-
-  const [completed, setCompleted]= useState(status)
+const Task = ({ title, description, status, taskId }) => {
+  const [completed, setCompleted] = useState(status);
 
   return (
-    <View>
+    <View style={styles.taskContainer}>
       <Text>Task</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  taskContainer: {
+    padding: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+});
