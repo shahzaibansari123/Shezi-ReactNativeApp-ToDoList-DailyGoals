c0bab4b581730bbd5b5394a5a77cf142f6f6ca84
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-10-11 22:19:21 +0500

    work complete

--- todoapp/screens/Home.jsx
+++ todoapp/screens/Home.jsx
@@ -5,9 +5,11 @@ import {
   StatusBar,
   SafeAreaView,
   StyleSheet,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import Task from "../components/Task";
+import Icon from "react-native-vactor-icons/Entypo";
 // import { useNavigation } from '@react-navigation/native'
 
 const Home = ({ navigation }) => {
@@ -38,6 +40,10 @@ const Home = ({ navigation }) => {
           <Task key={item._id} title={item.title} description={item.description} completed={item.completed} taskId={item._id}   />
         ))}
       </SafeAreaView>
+
+      <TouchableOpacity>
+        <Icon />
+      </TouchableOpacity>
     </View>
   );
 };
