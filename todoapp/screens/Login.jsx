cf025810d14bfeda07def242a03c6cf15f4f7f7d
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-11-02 21:27:45 +0500

    work complete

--- todoapp/screens/Login.jsx
+++ todoapp/screens/Login.jsx
@@ -1,17 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from "react-native";
+import React from "react";
 
 const Login = () => {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    }}>
-      <Text style={{fontSize: 20, margin: 20}}>Welcome</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 20, margin: 20 }}>Welcome</Text>
+      <View style={{ width: "70%" }}>
+        <TextInput placeholder="email" value={email} onChangeText={setEmail} />
+        <TextInput
+          placeholder="password"
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
