1e1b3472da4215a9b8b4a2093ecb0402a90a4c9d
Shahzaib Ansari <61605534+shahzaibansari123@users.noreply.github.com>
2022-11-30 19:39:11 +0500

    Work Complete

--- todoapp/screens/Camera.jsx
+++ todoapp/screens/Camera.jsx
@@ -2,9 +2,51 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Camera = () => {
+
+   
+
+
+
+
+
+
+
+
+  const [type, setType] = useState(CameraType.back);
+
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) ... 
+
+  if (!permission.granted) ... 
+
+  function toggleCameraType() {
+
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+
+  }
+
   return (
-    <View>
-      <Text>Camera</Text>
+
+    <View style={styles.container}>
+
+      <Camera style={styles.camera} type={type}>
+
+        <View style={styles.buttonContainer}>
+
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+
+            <Text style={styles.text}>Flip Camera</Text>
+
+          </TouchableOpacity>
+
+        </View>
+
+      </Camera>
+
+    </View>
+
+  );
     </View>
   )
 }
@@ -12,3 +54,5 @@ const Camera = () => {
 export default Camera
 
 const styles = StyleSheet.create({})
+
+
