15f9e7647eb2ac04611f8b7321f7cbb0a7609f5b
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-02 21:39:00 +0500

    work complete

--- todoapp/screens/Camera.jsx
+++ todoapp/screens/Camera.jsx
@@ -1,87 +1,36 @@
 //import { StyleSheet, Text, View } from 'react-native'
-import React from 'react' 
+import React from "react";
 
-
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
- 
- 
-      
-          
-        
-       
- 
- 
-
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Camera = () => {
-
-   
-
-
-
-
-
-
-
-
   const [type, setType] = useState(CameraType.back);
-
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  if (!permission) ... 
+  // if (!permission)
+  // if (!permission.granted)
 
-  if (!permission.granted) ... 
-
-  function toggleCameraType() {
-
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-
+  const toggleCameraType=()=> {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
   }
 
   return (
-
     <View style={styles.container}>
-
       <Camera style={styles.camera} type={type}>
-
         <View style={styles.buttonContainer}>
-
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-
             <Text style={styles.text}>Flip Camera</Text>
-
           </TouchableOpacity>
-
         </View>
-
       </Camera>
-
     </View>
-
   );
-    </View>
-  )
-}
+};
 
-export default Camera
+export default Camera;
 
-const styles = StyleSheet.create({})
-
-
+const styles = StyleSheet.create({});
