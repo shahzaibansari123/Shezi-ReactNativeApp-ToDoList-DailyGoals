962529fa1c670dbefd844fb214b7a713615799c6
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-13 22:22:57 +0500

    work complete

--- todoapp/screens/Cameraa.jsx
+++ todoapp/screens/Cameraa.jsx
@@ -1,17 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Camera, CameraType } from "expo-camera";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Cameraa = () => {
+  // const [permission, requestPermission] = useState(null);
+  // const [type, setType] = useState(CameraType.back);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Camera.requestCameraPermissionsAsync();
+  //     requestPermission(status === "granted");
+  //   })();
+  // }, []);
+
+  // if (!permission) {
+  //   return <Text>No Access to Camera</Text>;
+  // }
+
+  // if (permission === null) {
+  //   return <View />;
+  // }
+
+  // const toggleCameraType = () => {
+  //   setType((current) =>
+  //     current === CameraType.back ? CameraType.front : CameraType.back
+  //   );
+  // };
+
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
   if (!permission){
     return <Text>No Access to Camera</Text>
   }
-  
-  if (permission.granted){
-    requestPermission(true)
+
+  if (!permission.granted){
+    // requestPermission(false)
     return <View />
   }
 
@@ -22,11 +46,11 @@ const Cameraa = () => {
   }
 
   return (
-    <View style={{flex: 1}} >
-      <Camera type={type} style={{flex:1}}>
-        <View >
+    <View style={{ flex: 1 }}>
+      <Camera type={type} style={{ flex: 1 }}>
+        <View>
           <TouchableOpacity onPress={toggleCameraType}>
-            <Text >Flip Camera</Text>
+            <Text>Flip Camera</Text>
           </TouchableOpacity>
         </View>
       </Camera>
