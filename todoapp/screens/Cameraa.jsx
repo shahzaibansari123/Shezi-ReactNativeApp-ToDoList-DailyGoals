f9148c0e1db31c9ed8bcef0f6afb004daac0411f
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-16 05:03:23 +0500

    work complete

--- todoapp/screens/Cameraa.jsx
+++ todoapp/screens/Cameraa.jsx
@@ -1,80 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Camera, CameraType } from "expo-camera";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import Icon from 'react-native-vector-icons/MaterialIcons'
-
+import { StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const Cameraa = () => {
-  // const [permission, requestPermission] = useState(null);
-  // const [type, setType] = useState(CameraType.back);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const { status } = await Camera.requestCameraPermissionsAsync();
-  //     requestPermission(status === "granted");
-  //   })();
-  // }, []);
-
-  // if (!permission) {
-  //   return <Text>No Access to Camera</Text>;
-  // }
-
-  // if (permission === null) {
-  //   return <View />;
-  // }
-
-  // const toggleCameraType = () => {
-  //   setType((current) =>
-  //     current === CameraType.back ? CameraType.front : CameraType.back
-  //   );
-  // };
-
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [camera, setCamera] = useState(null);
-
-
 
   if (!permission) {
     return <Text>No Access to Camera</Text>;
   }
 
   if (!permission.granted) {
-    // requestPermission(false)
     return <View />;
   }
 
-  // const toggleCameraType = () => {
-  //   setType((current) =>
-  //     current === CameraType.back ? CameraType.front : CameraType.back
-  //   );
-  // };
+  const ImagePicker = () => {};
 
-  const ImagePicker=()=>{
-
-  }
-
-  const clickPicture=()=>{
-    
-  }
+  const clickPicture = () => {};
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera type={type} style={{ flex: 1 , aspectRatio: 1}} ratio="1:1" ref={(e)=> setCamera(e)} />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            width: "100%",
-            bottom: 10,
-            position: "absolute",
-          }}
-        >
-          <Icon name="image" size={40} color="#fff" onPress={ImagePicker}/>
-          <Icon name="camera" size={40} color="#fff" onPress={clickPicture}/>
-          <Icon name="flip-camera-android" size={40} color="#fff" onPress={()=> setType(type === CameraType.back ? CameraType.front : CameraType.back)}/>
-          </View>
-      
+      <Camera
+        type={type}
+        style={{ flex: 1, aspectRatio: 1 }}
+        ratio="1:1"
+        ref={(e) => setCamera(e)}
+      />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          width: "100%",
+          bottom: 10,
+          position: "absolute",
+        }}
+      >
+        <Icon name="image" size={40} color="#fff" onPress={ImagePicker} />
+        <Icon name="camera" size={40} color="#fff" onPress={clickPicture} />
+        <Icon
+          name="flip-camera-android"
+          size={40}
+          color="#fff"
+          onPress={() =>
+            setType(
+              type === CameraType.back ? CameraType.front : CameraType.back
+            )
+          }
+        />
+      </View>
     </View>
   );
 };
@@ -82,3 +57,29 @@ const Cameraa = () => {
 export default Cameraa;
 
 const styles = StyleSheet.create({});
+
+// older ways to implement Camera component
+
+// const [permission, requestPermission] = useState(null);
+// const [type, setType] = useState(CameraType.back);
+
+// useEffect(() => {
+//   (async () => {
+//     const { status } = await Camera.requestCameraPermissionsAsync();
+//     requestPermission(status === "granted");
+//   })();
+// }, []);
+
+// if (!permission) {
+//   return <Text>No Access to Camera</Text>;
+// }
+
+// if (permission === null) {
+//   return <View />;
+// }
+
+// const toggleCameraType = () => {
+//   setType((current) =>
+//     current === CameraType.back ? CameraType.front : CameraType.back
+//   );
+// };
