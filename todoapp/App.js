51b5e581d45506692cff8560a037f2fa4d6c9ed6
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-26 07:10:40 +0500

    work complete

--- todoapp/App.js
+++ todoapp/App.js
@@ -1,13 +1,11 @@
-import Main from "./Main"
-import { Provider } from "react-redux"
-import store from "./redux/store"
-
+import Main from "./Main";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      
-  <Main />
+      <Main />
     </Provider>
-  )
+  );
 }
