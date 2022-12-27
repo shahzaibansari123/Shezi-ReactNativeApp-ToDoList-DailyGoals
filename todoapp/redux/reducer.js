855027f4cea482d4ab57049efb721f54454076bc
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-27 05:05:30 +0500

    work complete

--- todoapp/redux/reducer.js
+++ todoapp/redux/reducer.js
@@ -1,7 +1,20 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 export const auth= createReducer({},{
-    loginRequest:(state,action)=>{},
-    loginSuccess:(state,action)=>{},
-    loginFailed:(state,action)=>{},
+    loginRequest:(state)=>{
+        state.loading= true;
+    },
+    loginSuccess:(state,action)=>{
+        state.loading =false;
+        state.isAuthenticated = true;
+        state.user= action.payload.user;
+        state.message= action.payload.message;
+        
+    },
+    loginFailed:(state,action)=>{
+        state.loading =false;
+        state.isAuthenticated = false;
+        state.error= action.payload.error;
+
+    },
 })
