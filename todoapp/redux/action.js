f9fc1dc7113cbecdf69271e8f0c5db2246385ef9
Shahzaib Ansari <61605534+shahzaibansari123@users.noreply.github.com>
2022-12-29 21:09:30 +0500

    Working on it

--- todoapp/redux/action.js
+++ todoapp/redux/action.js
@@ -1,9 +1,11 @@
-
+import axios from "axios"
 
 export const login = (email, password)=> async (dispatch)=>{
 try{
   
   dispatch({type: "loginRequest"})
+  
+  const {data}=await axios.post()
   } catch(error){
     }
 }
