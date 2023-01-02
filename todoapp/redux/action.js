8323ec4b32f82f2443703f6d52da30ffa9bedb98
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-01-02 07:22:25 +0500

    work complete

--- todoapp/redux/action.js
+++ todoapp/redux/action.js
@@ -1,18 +1,22 @@
-import axios from "axios"
+import axios from "axios";
 
-ccost serverUrl="";
-export const login = (email, password)=> async (dispatch)=>{
-try{
-  
-  dispatch({type: "loginRequest"})
-  
-  const {data}=await axios.post(`${serverUrl}/login`,{email, password,{
-                                                     headers:{
-                                                      "Content-Type" : "application/json",
-                                                      },
-                                                     })
-  
-  dispatch({type: "loginSuccess", payload: data})
-  } catch(error){
-    }
-}
+const serverUrl = "";
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: "loginRequest" });
+
+    const { data } = await axios.post(
+      `${serverUrl}/login`,
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch({ type: "loginSuccess", payload: data });
+  } catch (error) {
+    dispatch({ type: "loginFailed", payload: error.response.data.message });
+  }
+};
